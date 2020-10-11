f4b154e21ca17129c56f64d9aef3b76478226ae6
Dmitrij Rusakov <daemon2017@gmail.com>
2020-10-11 23:08:11 +0300

    Взят функционал из библиотеки

--- main.js
+++ main.js
@@ -171,16 +171,6 @@ function drawData(snpList) {
     }
 }
 
-function pearson(x, y) {
-    let promedio = (lista) => {
-        return lista.reduce((s, a) => s + a, 0) / lista.length
-    };
-    let n = x.length, prom_x = promedio(x), prom_y = promedio(y);
-    return (x.map((e, i, r) => (r = {x: e, y: y[i]})).reduce((s, a) => s + a.x * a.y, 0) - n * prom_x * prom_y) /
-        ((Math.sqrt(x.reduce((s, a) => (s + a * a), 0) - n * prom_x * prom_x)) *
-            (Math.sqrt(y.reduce((s, a) => (s + a * a), 0) - n * prom_y * prom_y)));
-}
-
 function makeTableHTML(myArray) {
     var result = "<table border=1>";
     for (var i = 0; i < myArray.length; i++) {
@@ -220,7 +210,7 @@ function getCorrelation() {
         for (var a = 0; a < dataList.length; a++) {
             var correlationRow = [];
             for (var b = 0; b < dataList.length; b++) {
-                correlationRow.push(pearson(countListList[a], countListList[b]).toFixed(2));
+                correlationRow.push(jStat.corrcoeff(countListList[a], countListList[b]).toFixed(2));
             }
             correlationMatrix.push(correlationRow);
         }
