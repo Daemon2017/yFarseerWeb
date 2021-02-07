99c95eda9e66a2e0b3021d4b61cc18b370536ca5
Dmitrij Rusakov <daemon2017@gmail.com>
2021-02-07 19:22:55 +0300

    Добавлена возможность сортировать столбцы таблицы

--- main.js
+++ main.js
@@ -495,9 +495,9 @@ function getHtmlTable(myArray, successSnpList) {
         if (i === 0) {
             result += "<thead>";
             result += "<tr>";
-            result += "<th>" + "</th>";
+            result += "<th onclick='sortTable(0)'>" + "</th>";
             row.forEach(function (_column, j) {
-                result += `<th>${successSnpList[j]}</th>`;
+                result += `<th onclick='sortTable(${j})'>${successSnpList[j]}</th>`;
             });
             result += "</tr>";
             result += "</thead>";
@@ -539,5 +539,42 @@ function getCorrelationClass(correlationValue) {
         return "highPos";
     } else if (correlationValue > 0.90) {
         return "veryHighPos";
+    }
+}
+
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("correlTable");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
     }
 }
